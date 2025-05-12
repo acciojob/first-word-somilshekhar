6aeaa3ac8ae92feae4e4acc456af2c89d2fc742d
somilshekhar <129623166+somilshekhar@users.noreply.github.com>
2025-05-12 13:06:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function firstWord(s) {
-  // your code here
+  // Trim to remove leading/trailing spaces
+  s = s.trim();
+
+  // Find the index of the first space
+  const spaceIndex = s.indexOf(' ');
+
+  // If no space, return the entire string
+  if (spaceIndex === -1) {
+    return s;
+  }
+
+  // Return substring up to first space
+  return s.substring(0, spaceIndex);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
